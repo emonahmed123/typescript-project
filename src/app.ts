bd60054ff9fbcace8a633ffa7eb3004b05e3090b
Emon Ahmed <imonshomon@gmail.com>
2024-06-28 16:53:49 +0600

    projects setup

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express, { Request, Application, Response } from 'express';
+
+import cors from 'cors';
+
+const app: Application = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+export default app;
