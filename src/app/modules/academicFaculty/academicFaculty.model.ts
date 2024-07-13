cf29c614cac7f22738b99c85dfaba015d7dc136d
Emon Ahmed <imonshomon@gmail.com>
2024-07-13 11:29:08 +0600

    create academicFaculty

---
+++ src/app/modules/academicFaculty/academicFaculty.model.ts
@@ -0,0 +1,21 @@
+import { Schema, model } from 'mongoose';
+
+import bcrypt from 'bcrypt';
+import config from '../../config';
+import { TAcademicFaculty } from './academicFaculty.interface';
+
+const academicFacultySchema = new Schema<TAcademicFaculty>(
+  {
+    name: {
+      type: String,
+      require: true,
+      unique:true
+    },
+
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const AcademicFaculty = model<TAcademicFaculty>('AcademicFaculty', academicFacultySchema);
